a3db1f1880c9a2d5133e36563fbf471e7e832d0f
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2022-06-17 18:15:55 +0300

    * Fixed http error

---
+++ lib/managers/http_manager.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+
+class HttpManager extends HttpOverrides with ChangeNotifier{
+  static final HttpManager _singleton = HttpManager._internal();
+
+  factory HttpManager() {
+    return _singleton;
+  }
+
+  HttpManager._internal();
+
+  Future<void> init() async {
+    HttpOverrides.global = HttpManager();
+  }
+
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
